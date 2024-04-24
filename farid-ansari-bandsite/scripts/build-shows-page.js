77d3031c0bf3cd0aa737c11b5c3552f601bcc87e
farid.ansari <farid.ansari@gmail.com>
2024-04-24 02:06:05 -0700

    Implemented basic DOM UPDATE functionality for both pages.

--- farid-ansari-bandsite/scripts/build-shows-page.js
+++ farid-ansari-bandsite/scripts/build-shows-page.js
@@ -40,13 +40,132 @@ let bandsiteShows = {
       showLocation: "San Fransico, CA",
     };
 
-    console.info("Shows objects initialized with following default values:");
-    console.table(this.shows);
+    for (var i = 0; i < (this.shows.length); i++)
+    {
+      if (i==0)
+        this.updatePage(this.getShow(i), true);
+      else
+        this.updatePage(this.getShow(i));
+    }
+
+
   },
   getShow: function (showID) {
-    console.log("Returning Show ID: " + showID);
-    console.table(this.shows[showID]);
+    return this.shows[showID];
+
   },
+
+
+  updatePage: function ({ showDate, showVenue, showLocation }, isfirstRow = false) {
+    var childElement;
+    var parentElement;
+
+    parentElement = document.getElementById("showsOutputContainer");
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col__label");
+    if (isfirstRow == true)
+      childElement.classList.add("shows__output__row__col__label--firstrow");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__label");
+    childElement.innerText = "Date";
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col__content");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__commentsOutput");
+    childElement.classList.add("type__commentsOutput--name");
+    childElement.innerText = showDate;
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col__label");
+    if (isfirstRow == true)
+      childElement.classList.add("shows__output__row__col__label--firstrow");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__label");
+    childElement.innerText = "Venue";
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col__content");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__commentsOutput");
+    childElement.innerText = showVenue;
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col__label");
+    if (isfirstRow == true)
+      childElement.classList.add("shows__output__row__col__label--firstrow");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__label");
+    childElement.innerText = "Location";
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__col__content");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__commentsOutput");
+    childElement.innerText = showLocation;
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("shows__output__row__button");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("input");
+    childElement.classList.add("type__inputButton");
+    childElement.setAttribute("type", "submit");
+    childElement.setAttribute("value", "BUY TICKETS");
+    parentElement.appendChild(childElement);
+  },
+
 };
 
 bandsiteShows.initalizeShows();
--- farid-ansari-bandsite/scripts/index-page.js
+++ farid-ansari-bandsite/scripts/index-page.js
@@ -7,27 +7,6 @@ let bandsiteComments = {
     },
   ],
 
-  postComment: function (strUsername, strComment) {
-    this.comments.push({
-      username: strUsername,
-      timestamp: Date.now(),
-      comment: strComment,
-    });
-    console.log("Comment Posted.");
-  },
-
-  getComment: function (commentID) {
-    let singleCommment = {
-      username: this.comments[commentID].username,
-      timestamp: this.comments[commentID].timestamp,
-      comment: this.comments[commentID].comment,
-      relativeTime: this.getRelativeTime(this.comments[commentID].timestamp),
-    };
-
-    console.log("Returning Comment ID: " + commentID);
-    console.table(singleCommment);
-  },
-
   initalizeComments: function () {
     this.comments[0] = {
       username: "Victor Pinto",
@@ -47,69 +26,195 @@ let bandsiteComments = {
       comment:
         "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
     };
-    console.info("Comments objects initialized with following default values:");
-    console.table(this.comments);
+  
+    for (var i = (this.comments.length)-1; i >= 0; i--)
+    {
+        this.updatePage(this.getComment(i));
+    }
+  },
+
+  postComment: function (strUsername, strComment) {
+    let i = this.comments.push({
+      username: strUsername,
+      timestamp: Date.now(),
+      comment: strComment,
+    });
+    return i - 1;
+  },
+
+  getComment: function (commentID) {
+    let singleCommment = {
+      username: this.comments[commentID].username,
+      relativeTime: this.getRelativeTime(this.comments[commentID].timestamp),
+      comment: this.comments[commentID].comment,
+    };
+    return singleCommment;
   },
 
   getRelativeTime: function (time) {
+    /*
+      Got original function from: https://codereview.stackexchange.com/questions/44623/output-human-readable-time. 
+      Made the following improvements/ modifictions:
+      1) Support for singuar and plural (month vs months)
+      2) Support for year (original script went only up to month)
+      3) Using Floor instead of Round function to round avoid round up
+      4) Implemented remainder display (example: instead of rounding to "about one year ago", it can display "about one year and 5 months")
+      5) moved related variables into one object to make it cleaner.
+    */
+
+    let terminate = false;
     let SECOND_MS = 1000;
     let MINUTE_MS = 60 * SECOND_MS;
     let HOUR_MS = 60 * MINUTE_MS;
     let DAY_MS = 24 * HOUR_MS;
     let WEEK_MS = 7 * DAY_MS;
     let MONTH_MS = 30 * DAY_MS;
-    let YEAR_MS = 12 * MONTH_MS; //Note: years are a bit buggy for some reason (added it myself)
+    let YEAR_MS = 12 * MONTH_MS;
 
-    let lookup_plural = [
-      "years",
-      "months",
-      "weeks",
-      "days",
-      "hours",
-      "minutes",
-      "seconds",
-    ];
-    let lookup_singular = [
-      "year",
-      "month",
-      "week",
-      "day",
-      "hour",
-      "minute",
-      "second",
-    ];
-    let values = [];
+    let lookup = {
+      plural: [
+        "years",
+        "months",
+        "weeks",
+        "days",
+        "hours",
+        "minutes",
+        "seconds",
+      ],
+      singular: ["year", "month", "week", "day", "hour", "minute", "second"],
+      maxValues: [999, 12, 4, 30, 24, 60, 60],
+      values: [],
+    };
+
     let ms = Date.now() - time;
 
-    values.push(ms / YEAR_MS);
+    lookup.values.push(ms / YEAR_MS);
     ms %= YEAR_MS;
-    values.push(ms / MONTH_MS);
+    lookup.values.push(ms / MONTH_MS);
     ms %= MONTH_MS;
-    values.push(ms / WEEK_MS);
+    lookup.values.push(ms / WEEK_MS);
     ms %= WEEK_MS;
-    values.push(ms / DAY_MS);
+    lookup.values.push(ms / DAY_MS);
     ms %= DAY_MS;
-    values.push(ms / HOUR_MS);
+    lookup.values.push(ms / HOUR_MS);
     ms %= HOUR_MS;
-    values.push(ms / MINUTE_MS);
+    lookup.values.push(ms / MINUTE_MS);
     ms %= MINUTE_MS;
-    values.push(ms / SECOND_MS);
+    lookup.values.push(ms / SECOND_MS);
     ms %= SECOND_MS;
 
-    let prettyTime = "about ";
+    let prettyTime = "";
 
-    for (var i = 0; i < values.length; i++) {
-      var val = Math.floor(values[i]); // changed to floor from ROUND but that might cause some issues with years
-      if (val <= 0) continue;
+    for (var i = 0; i < lookup.values.length; i++) {
+      var val = Math.floor(lookup.values[i]);
 
-      if (val == 1) prettyTime += val + " " + lookup_singular[i] + " ago";
-      else prettyTime += val + " " + lookup_plural[i] + " ago";
+      if (val <= 0) {
+        //if the current time unit is 0, skip loop to next lower unit
+        continue;
+      }
 
+      if (val == 1) {
+        // get singular time unit name
+        prettyTime += val + " " + lookup.singular[i];
+      } else {
+        // get plural time unit name
+        prettyTime += val + " " + lookup.plural[i];
+      }
+
+      // This block allows the appending of the next lower time unit if relevant conditions are met by allowing the loop to contine one more time
+      if (
+        lookup.values[i] - Math.floor(lookup.values[i]) > 0 &&
+        Math.floor(lookup.values[i + 1] > 0) &&
+        lookup.maxValues[i + 1] != Math.floor(lookup.values[i + 1]) &&
+        terminate == false
+      ) {
+        prettyTime += " and ";
+        terminate = true;
+        continue;
+      }
       break;
     }
-    return prettyTime;
+    return prettyTime + " ago";
+  },
+
+  updatePage: function ({ username, relativeTime, comment }) {
+    var childElement;
+    var parentElement;
+
+    parentElement = document.getElementById("commentsOutputContainer");
+    childElement = document.createElement("div");
+    childElement.classList.add("comments__output__row");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("comments__output__row__left");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("img");
+    childElement.classList.add("comments__output__row__left__img");
+    childElement.setAttribute("src", "./assets/images/Mohan-muruge.jpg");
+    childElement.setAttribute("alt", "user");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("comments__output__row__right");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("comments__output__row__right__content");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("div");
+    childElement.classList.add("comments__output__row__right__content__header");
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("div");
+    childElement.classList.add(
+      "comments__output__row__right__content__header__name"
+    );
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__commentsOutput");
+    childElement.classList.add("type__commentsOutput--name");
+    childElement.innerText = username;
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add(
+      "comments__output__row__right__content__header__date"
+    );
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__commentsOutput");
+    childElement.classList.add("type__commentsOutput--date");
+    childElement.innerText = relativeTime;
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement.parentElement.parentElement.parentElement;
+    childElement = document.createElement("div");
+    childElement.classList.add(
+      "comments__form__comment__cont__right__content__text"
+    );
+    parentElement.appendChild(childElement);
+
+    parentElement = childElement;
+    childElement = document.createElement("p");
+    childElement.classList.add("type__commentsOutput");
+    childElement.classList.add("type__commentsOutput--comment");
+    childElement.innerText = comment;
+    parentElement.appendChild(childElement);
   },
 };
 
 bandsiteComments.initalizeComments();
-bandsiteComments.postComment("Farid", "This is Great YOOO!");
